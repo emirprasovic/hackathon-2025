116711cdce8a53d4efbc81d167a31a5868a89912
AhmedKedic7 <ahmedkedic7@gmail.com>
2025-02-22 10:27:23 +0100

    minor changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LandingPage from "./components/landing/LandingPage";
 import Chat from "./components/chat/Chat";
 import Header from "./components/header/Header";
+import DonatePage from './components/donate-page/DonatePage';
 import './App.css'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/donate" element={<DonatePage />} />
       </Routes>
     </Router>
   )
