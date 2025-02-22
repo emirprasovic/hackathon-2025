1d49c37eed884b9c037e1877de8e0b6ef07c4e2d
Criptonnn <eemaemir1@gmail.com>
2025-02-22 10:31:08 +0100

    Minor folder ttweaks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import LandingPage from "./components/landing/LandingPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/landing/LandingPage";
 import Chat from "./components/chat/Chat";
 import Header from "./components/header/Header";
-import DonatePage from './components/donate-page/DonatePage';
-import './App.css'
+import DonatePage from "./pages/donate/DonatePage";
+import "./App.css";
 
 function App() {
   return (
     <Router>
       <Chat />
       <Header />
-      
+
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/donate" element={<DonatePage />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
